418fc7175aceb8d78bc1e6c24030ce6373980f04
Sam Greenhalgh <github.com@zapthedingbat.com>
2019-03-01 15:09:24 +0000

    add logger

---
+++ src/logger.js
@@ -0,0 +1,20 @@
+const Pino = require('pino');
+
+const options = {
+  customLevels: {
+    trace: 10,
+    debug: 20,
+    info: 30,
+    warning: 40,
+    error: 50,
+    fatal: 60,
+  },
+  useOnlyCustomLevels: true,
+  level: process.env.LOG_LEVEL || 'info',
+  prettyPrint: process.env.NODE_ENV !== 'production',
+  name: process.env.npm_package_name,
+};
+
+const pino = new Pino(options);
+
+module.exports = pino;
